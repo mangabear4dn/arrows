148372472b12987b3cf35525f4ac07bf7ef021b8
mangabear4dn <luize.blake@gmail.com>
2022-03-18 11:23:43 +0000

    Solutions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     return a+b;
+// }
+// let sum=addTwoNumbers(3, 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a+b;
+}
+let sum=addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => (a + b); //iekavas izn'akumam ir optional
+
+let sum2 = addTwoNumbers2(4, 6);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+
+saySomething("Hello");
+
+const sayHei = () => console.log("Hei");
+
+sayHei();
+
 // Returning Multiple Lines
+
+const returnMultiLine = () => (
+    `<p>
+        Hello there!
+    </p>`
+)
+
+console.log(returnMultiLine());
